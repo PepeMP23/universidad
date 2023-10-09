767965d29b759468f3129ad348aab137c2ad8cb1
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-10-08 22:08:42 -0600

    Correccion vista admin

--- universidad/estudiantes/models.py
+++ universidad/estudiantes/models.py
@@ -5,20 +5,24 @@ class Estudiante(models.Model):
     nombres = models.CharField(max_length=255)
     fecha_inscripcion = models.DateField(null=True)
 
-    def _str_(self):
+    def __str__(self):
         return f"{self.nombres} {self.apellidos}"
 
 class Curso(models.Model):
     titulo = models.CharField(max_length=200)
     creditos = models.IntegerField(null=True)
 
-    def _str_(self):
+    def __str__(self):
         return self.titulo
 
 class Inscripcion(models.Model):
-    id_curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    id_curso = models.ForeignKey(
+        Curso, null=True, blank=True, on_delete=models.CASCADE)
     id_estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
 
+    class Meta:
+        # Gives the proper plural name for admin
+        verbose_name_plural = "Inscripciones"
 
     def __str__(self):
-        return f"{self.id_curso} {self.id_estudiante}"
+        return Estudiante
